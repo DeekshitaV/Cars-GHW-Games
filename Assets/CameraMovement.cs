b0fa9c285204cad65e94cef17737b1f2dafa3a57
Deekshita Verma <deekshitaverma@gmail.com>
2023-05-03 17:24:03 +0530

    Initial commit

---
+++ Assets/CameraMovement.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    public Transform carPosition;
+    private float difference;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        difference = carPosition.position.z - transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = transform.position.x;
+        float y = transform.position.y;
+        float z = carPosition.position.z - difference;
+        transform.position = new Vector3(x,y,z);
+    }
+}
---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float moveSpeed = 12f;
+    public float turnSpeed = 180f;
+    private Rigidbody rigidbody;
+
+    private float movementInputValue;
+    private float turnInputValue;
+
+    void Awake(){
+       rigidbody = GetComponent<Rigidbody>();
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody.isKinematic = false;
+
+        movementInputValue = 0f;
+        turnInputValue = 0f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movementInputValue = Input.GetAxis("Vertical");
+        turnInputValue = Input.GetAxis("Horizontal");
+
+        Vector3 movement = transform.forward * moveSpeed * movementInputValue * Time.deltaTime;
+        rigidbody.MovePosition(rigidbody.position + movement);
+
+        float turn = turnInputValue * turnSpeed * Time.deltaTime;
+        Quaternion turnRotation = Quaternion.Euler(0f,turn,0f);
+        rigidbody.MoveRotation(rigidbody.rotation * turnRotation);
+    }
+}
